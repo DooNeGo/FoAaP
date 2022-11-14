8206b17b35aaa1ae468ee1cbc58e5752dfc5f05d
Mathew <113514856+DooNeGo@users.noreply.github.com>
2022-11-14 12:43:48 +0300

    Update lab3.1.c

--- Lab 3/1/lab3.1.c
+++ Lab 3/1/lab3.1.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(void){
-	float a,b,c,n;
+	float a, b=20, n;
 	printf("Enter the current year: ");
 	scanf("%f", &a);
-	b=20;
-	for(n=1626; n<=a; n++)
-	{
-		b=b+b*0.04;
-	}
+	for(n=1626; n<=a; n++) b=b+b*0.04;
 	printf("b: %0.f\n", b);
 	system("pause");
 	return 0;
