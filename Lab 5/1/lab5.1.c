6d4400e5ab80d9703d4ddc97b69af14836f059c1
Mathew <113514856+DooNeGo@users.noreply.github.com>
2022-11-22 00:18:14 +0300

    Add files via upload

--- Lab 5/1/lab5.1.c
+++ Lab 5/1/lab5.1.c
@@ -7,31 +7,31 @@ int main(){
   printf("Enter dimension of 2D matrix: ");
   scanf("%d %d", &n, &m);
   int a[n][m], u[n], s[m];
-  for(i=0; i<n; i++) u[i]=0;
+  for (i=0; i<n; i++) u[i]=0;
   printf("Generated matrix: \n");
-  for(i=0; i<n; i++){
-    for(j=0; j<m; j++){
+  for (i=0; i<n; i++){
+    for (j=0; j<m; j++){
       a[i][j]=rand()%13+(-3);
       printf("%3d",a[i][j]);
     }
     printf("\n");
   }
-  for(i=0; i<n; i++){
-    for(j=0; j<m-1; j++){
-      for(k=j+1; k<m; k++){
-        if(a[i][j]==a[i][k]) v++;
+  for (i=0; i<n; i++){
+    for (j=0; j<m-1; j++){
+      for (k=j+1; k<m; k++){
+        if (a[i][j]==a[i][k]) v++;
       }
-      if(v>u[i]) u[i]=v;
+      if (v>u[i]) u[i]=v;
       v=1;
     }
   }
   printf("\nThe number of repeated elements in a row: \n");
-  for(i=0; i<n; i++)
+  for (i=0; i<n; i++)
     printf("%d\n",u[i]);
-  for(i=0; i<n-1; i++){
-    for(v=i+1; v<n; v++){
+  for (i=0; i<n-1; i++){
+    for (v=i+1; v<n; v++){
       if (u[i]>u[v]){
-        for(k=0; k<m; k++){
+        for (k=0; k<m; k++){
           s[k]=a[i][k];
           a[i][k]=a[v][k];
           a[v][k]=s[k];
@@ -40,27 +40,27 @@ int main(){
     }
   }
   printf("\nSorted Matrix: \n");
-  for(i=0; i<n; i++){
-    for(j=0; j<m; j++){
+  for (i=0; i<n; i++){
+    for (j=0; j<m; j++){
       printf("%3d",a[i][j]);
     }
   printf("\n");
   }
-  for(j=0; j<=i; j++){
-    for(i=0; i<k; i++){
-      if(a[i][j]>0){
+  for (j=0; j<=i; j++){
+    for (i=0; i<k; i++){
+      if (a[i][j]>0){
         k++;
-        if(k=m){
+        if (k=m){
           j++;
           printf("The number of the column that doesn't contain any negative elements: %d\n", j);
           break;
         }
       }
-      else if(j){
+      else if (j){
         printf("\nNo negative elements\n");
         break;
       }
-      else if(a[i][j]<0) k=0;
+      else if (a[i][j]<0) k=0;
     }
   }
   system("pause");
