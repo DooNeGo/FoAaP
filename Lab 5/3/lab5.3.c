c35f6e3bf054f8766feef821ced4a6bd9d104fe4
Mathew <113514856+DooNeGo@users.noreply.github.com>
2022-12-01 23:16:39 +0300

    Some innovations

--- Lab 5/3/lab5.3.c
+++ Lab 5/3/lab5.3.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-void matrix_generate(int *matrix, int *rows, int *columns){
-    srand(time(0));
-    for (int i=0; i<*rows; i++){
-        for (int j=0; j<*columns; j++){
-            *(matrix+i**columns+j)=rand()%11-2;
+int findNum_firstRow_ordered_ascending_order(int *matrix, int *rows, int *columns){
+    int counter, i, j, k=-1, *row_num=&k;
+    for (i=0; i<*rows; i++){
+        counter=0;
+        for (j=0; j<=counter; j++){
+            if (*(matrix+i**columns+j)<*(matrix+i**columns+j+1)){
+                counter++;
+                if (counter==*columns-1){
+                    *row_num=i;
+                    i++;
+                    printf("Row number whose elements are sorted in ascending order: %d\n", i);
+                    goto jump;
+                }
+            }
+            else if (*(matrix+i**columns+j)>*(matrix+i**columns+j+1)){
+                counter=0;
+            }
         }
     }
+    jump:
+    return *row_num;
 }
-void matrix_input_user(int *matrix, int *rows, int *columns){
-    for (int i=0; i<*rows; i++){
-        for (int j=0; j<*columns; j++){
-            scanf("%d", (matrix+i**columns+j));
+int matrix_generate(int *matrix, int *rows, int *columns){
+    srand(time(0));
+    int row_num=-1, i, j;
+    while (row_num==-1){
+        for (i=0; i<*rows; i++){
+            for (j=0; j<*columns; j++){
+                *(matrix+i**columns+j)=rand()%14-2;
+            }
         }
+        row_num=findNum_firstRow_ordered_ascending_order(matrix, rows, columns);
     }
+    return row_num;
 }
 void matrix_output(int *matrix, int *rows, int *columns){
     for (int i=0; i<*rows; i++){
@@ -24,41 +44,16 @@ void matrix_output(int *matrix, int *rows, int *columns){
         printf("\n");
     }
 }
-int findNum_firstRow_ordered_ascending_order(int *matrix, int *rows, int *columns){
-    int counter, i, j, *row_num;
-    for (i=0; i<*rows; i++){
-        counter=0;
-        for (j=0; j<=counter; j++){
-            if (*(matrix+i**columns+j)<*(matrix+i**columns+j+1)){
-                counter++;
-                if (counter==*columns-1){
-                    *row_num=i;
-                    i++;
-                    printf("\nRow number whose elements are sorted in ascending order: %d\n", i);
-                    goto jump;
-                }
-            }
-            else if (*(matrix+i**columns+j)>*(matrix+i**columns+j+1)){
-                counter=0;
-            }
-        }
-    }
-    printf("\nNone of the rows are sorted in ascending order");
-    *row_num=-1;
-    jump:
-    printf("\n");
-    return *row_num;
-}
 void reverse_orderElem(int *matrix, int *columns, int *row_num){
     int temp, j, i=*row_num, k, max;
     for (j=0; j<*columns-1; j++){
         max=j;
         for (k=j+1; k<*columns; k++){
-            if (*(matrix+i**columns+j)<*(matrix+i**columns+k)){
+            if (*(matrix+i**columns+max)<*(matrix+i**columns+k)){
                 max=k;
             }
         }
-        if (max!=j){
+        if (max!=i){
             temp=*(matrix+i**columns+j);
             *(matrix+i**columns+j)=*(matrix+i**columns+max);
             *(matrix+i**columns+max)=temp;
@@ -66,33 +61,16 @@ void reverse_orderElem(int *matrix, int *columns, int *row_num){
     }
 }
 int main(){
-    int n, m, row_num, select_num;
+    int n, m, row_num;
     printf("Enter dimension of 2D matrix: ");
     scanf("%d%d", &n, &m);
     int a[n][m], a1[m], *matrix=&a[0][0];
-    printf("1 - Enter matrix manually\n2 - Generate matrix\nEnter the value of your choice: ");
-    scanf("%d", &select_num);
-    switch (select_num){
-    case 1:
-        printf("Enter %d-by-%d Matrix Values: \n", n, m);
-        matrix_input_user(matrix, &n, &m);
-        printf("\nEntered matrix: \n");
-        break;
-    case 2:
-        printf("Generated matrix: \n");
-        matrix_generate(matrix, &n ,&m);
-        break;
-    default:
-        printf("Wrong number");
-        break;
-    }
+    row_num=matrix_generate(matrix, &n ,&m);
+    printf("Generated matrix: \n");
     matrix_output(matrix, &n, &m);
-    row_num=findNum_firstRow_ordered_ascending_order(matrix, &n, &m);
-    if (row_num!=-1){
-        printf("New matrix: \n");
-        reverse_orderElem(matrix, &m, &row_num);
-        matrix_output(matrix, &n, &m);
-    }
+    printf("New matrix: \n");
+    reverse_orderElem(matrix, &m, &row_num);
+    matrix_output(matrix, &n, &m);
     system("pause");
     return 0;
 }
