cd4c8dc1de17b6e2268d017bae906dfd2adceab3
Mathew <113514856+DooNeGo@users.noreply.github.com>
2023-02-14 22:36:18 +0300

    Add files via upload

---
+++ Lab 7/2/lab7.2.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void counttheNumberofSpaces(char *string)
+{
+    int counter = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (string[i] == '\000')
+        {
+            break;
+        }
+        else
+        {
+            if (string[i] == ' ')
+            {
+                counter++;
+            }
+        }
+    }
+    printf("Number of spaces: %d\n", counter);
+}
+char *writeString()
+{
+    char ch;
+    char *string = (char *)malloc(sizeof(char) * 100);
+    for (int i = 0; i < 100; i++)
+    {
+        scanf("%c", &ch);
+        if (ch != '\n')
+        {
+            string[i] = ch;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return string;
+}
+int main()
+{
+    printf("Enter the string: ");
+    char *string = writeString();
+    counttheNumberofSpaces(string);
+    system("pause");
+    return 0;
+}
