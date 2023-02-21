57bc59a90fc8b8eef72fb222399a7fddc9d5135c
Mathew <113514856+DooNeGo@users.noreply.github.com>
2023-02-21 18:51:36 +0300

    Fixed bugs. Release

--- Lab 9/1/lab9.c
+++ Lab 9/1/lab9.c
@@ -1,57 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
+
 void showMessage(const char *msg, const char *color)
 {
     system("cls");
+
     if (color == "Green")
         system("color A");
-    else
+    else if (color == "Red")
         system("color 4");
-    fflush(stdin);
+
     printf("%s\n", msg);
+
     system("pause");
     system("color 7");
 }
-int load(int *temp, int *returnedSize)
+
+int *load(int *returnedSize)
 {
     int i = 0;
     int counter = 1;
+    int *temp = (int *)malloc(sizeof(int) * 10);
     FILE *f = fopen("f.txt", "rt");
+
     if (!f)
     {
         showMessage("ERROR1 file", "Red");
         fclose(f);
-        return 1;
+        temp[0] = 1;
+        return temp;
     }
     for (i = 0; !feof(f); i++)
     {
-        if (i % 10 == 0)
+        if (i % 10 == 0 && i > 0)
         {
             counter++;
-            do
-            {
-                temp = (int *)realloc(temp, sizeof(int) * counter * 10);
-            } while (!temp);
-            fscanf(f, "%d", &temp[i]);
-            if (temp[i] < 10 && temp[i] > -10 || temp[i] > 99 || temp[i] < -99)
-            {
-                showMessage("ERROR2 file", "Red");
-                fclose(f);
-                return 1;
-            }
+            temp = (int *)realloc(temp, sizeof(int) * counter * 10);
+        }
+
+        fscanf(f, "%d", &temp[i]);
+
+        if (temp[i] < 10 && temp[i] > -10 || temp[i] > 99 || temp[i] < -99)
+        {
+            showMessage("ERROR2 file", "Red");
+            fclose(f);
+            temp[0] = 1;
+            return temp;
         }
     }
     *returnedSize = i;
     fclose(f);
-    return 0;
+    return temp;
 }
+
 void createNewSpecificFile(int *temp, int size)
 {
     int K;
+    FILE *g = fopen("g.txt", "wt");
+
     printf("Enter K: ");
     scanf("%d", &K);
-    FILE *g = fopen("g.txt", "wt");
+
     for (int i = 0; i < size; i++)
     {
         if (temp[i] > K)
@@ -59,20 +68,21 @@ void createNewSpecificFile(int *temp, int size)
             fprintf(g, "%d\n", temp[i]);
         }
     }
+    fflush(g);
+    fclose(g);
     showMessage("The new file was created successfully", "Green");
 }
+
 int main()
 {
-    int *temp;
-    do
-    {
-        temp = (int *)malloc(sizeof(int) * 10);
-    } while (!temp);
     int sizeArray;
-    if (!load(temp, &sizeArray))
+    int *temp = load(&sizeArray);
+
+    if (temp[0] != 1)
     {
         createNewSpecificFile(temp, sizeArray);
     }
+
     free(temp);
     return 0;
 }
