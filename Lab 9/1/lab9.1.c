1b64a80cee68789bcdb9695016ce6caf20e9a0e8
Mathew <113514856+DooNeGo@users.noreply.github.com>
2023-02-23 01:40:48 +0300

    Code formatting changed

--- Lab 9/1/lab9.1.c
+++ Lab 9/1/lab9.1.c
@@ -4,14 +4,13 @@
 void showMessage(const char *msg, const char *color)
 {
     system("cls");
-
     if (color == "Green")
         system("color A");
+
     else if (color == "Red")
         system("color 4");
 
     printf("%s\n", msg);
-
     system("pause");
     system("color 7");
 }
@@ -22,7 +21,6 @@ int *load(int *returnedSize)
     int counter = 1;
     int *temp = (int *)malloc(sizeof(int) * 10);
     FILE *f = fopen("f.txt", "rt");
-
     if (!f)
     {
         showMessage("ERROR1 file", "Red");
@@ -32,14 +30,12 @@ int *load(int *returnedSize)
     }
     for (i = 0; !feof(f); i++)
     {
-        if (i % 10 == 0 && i > 0)
+        if (i != 0 && i % 10 == 0)
         {
             counter++;
             temp = (int *)realloc(temp, sizeof(int) * counter * 10);
         }
-
         fscanf(f, "%d", &temp[i]);
-
         if (temp[i] < 10 && temp[i] > -10 || temp[i] > 99 || temp[i] < -99)
         {
             showMessage("ERROR2 file", "Red");
@@ -57,10 +53,8 @@ void createNewSpecificFile(int *temp, int size)
 {
     int K;
     FILE *g = fopen("g.txt", "wt");
-
     printf("Enter K: ");
     scanf("%d", &K);
-
     for (int i = 0; i < size; i++)
     {
         if (temp[i] > K)
@@ -77,12 +71,10 @@ int main()
 {
     int sizeArray;
     int *temp = load(&sizeArray);
-
     if (temp[0] != 1)
     {
         createNewSpecificFile(temp, sizeArray);
     }
-
     free(temp);
     return 0;
 }
