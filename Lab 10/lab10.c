db0fc37b8666d97e1ced36181914c126c0b0331f
Mathew <113514856+DooNeGo@users.noreply.github.com>
2023-03-01 15:12:07 +0300

    Add function getRoots

--- Lab 10/lab10.c
+++ Lab 10/lab10.c
@@ -65,14 +65,13 @@ float *getMultipliedRow(float *matrix, int row, int columns, int rows, int *coun
     return tempArray;
 }
 
-float *doGaus(float *matrix, int rows, int columns)
+float *doGauss(float *matrix, int rows, int columns)
 {
     float *newMatrix = copyArray(matrix, rows, columns);
-    float temp;
     for (int i = 0; i < rows; i++)
     {
         int counter = i + 1;
-        temp = *(newMatrix + i * columns + i);
+        float temp = *(newMatrix + i * columns + i);
         divideRow(newMatrix, temp, columns, i);
         // showMatrix(newMatrix, 3, 4);
         for (int k = i + 1; k < rows; k++)
@@ -90,6 +89,23 @@ float *doGaus(float *matrix, int rows, int columns)
     return newMatrix;
 }
 
+float *getRoots(float *matrix, int rows, int columns)
+{
+    float *roots = (float *)malloc(sizeof(float) * rows);
+    int k = columns - 1;
+    for (int i = rows - 1; i >= 0; i--)
+    {
+        float temp = 0;
+        for (int j = k; j < columns - 1; j++)
+        {
+            temp += *(matrix + i * columns + j) * roots[j];
+        }
+        k--;
+        roots[i] = *(matrix + i * columns + columns - 1) - temp;
+    }
+    return roots;
+}
+
 int main()
 {
     int rows;
@@ -99,18 +115,26 @@ int main()
     scanf("%d%d", &rows, &columns);
 
     float *matrix = getMatrix(rows, columns);
-    //float array[12] = {1.14, -2.15, -5.11, -4.16, -0.71, 0.81, -0.02, -0.17, 0.42, -1.13, 7.05, 6.15};
-    float *newMatrix = doGaus(matrix, rows, columns);
+    // float array[12] = {1.14 -2.15 -5.11 -4.16 -0.71 0.81 -0.02 -0.17 0.42 -1.13 7.05 6.15};
+    float *newMatrix = doGauss(matrix, rows, columns);
 
+    printf("\nEntered matrix:\n");
     showMatrix(matrix, rows, columns);
     printf("New matrix:\n");
     showMatrix(newMatrix, rows, columns);
 
-    float x3 = *(newMatrix + 2 * columns + 3);
-    float x2 = *(newMatrix + 1 * columns + 3) - *(newMatrix + 1 * columns + 2) * x3;
-    float x1 = *(newMatrix + 0 * columns + 3) - *(newMatrix + 0 * columns + 1) * x2 - *(newMatrix + 0 * columns + 2) * x3;
+    //float x3 = *(newMatrix + 2 * columns + 3);
+    //float x2 = *(newMatrix + 1 * columns + 3) - *(newMatrix + 1 * columns + 2) * x3;
+    //float x1 = *(newMatrix + 0 * columns + 3) - *(newMatrix + 0 * columns + 1) * x2 - *(newMatrix + 0 * columns + 2) * x3;
+    //printf("x1: %f, x2: %f, x3: %f\n", x1, x2, x3);
 
-    printf("\nx1: %f, x2: %f, x3: %f\n", x1, x2, x3);
+    float *roots = getRoots(newMatrix, rows, columns);
+    for (int i = 0; i < rows; i++)
+    {
+        printf("x%d: %f  ", i + 1, roots[i]);
+    }
+    printf("\n");
+
     free(matrix);
     free(newMatrix);
     system("pause");
