e6241959b9c3f87ce714698dfc941e0f0954efb1
Mathew <113514856+DooNeGo@users.noreply.github.com>
2023-03-12 02:01:06 +0300

    Changed everything that Stas asked

--- Lab 10/lab10.c
+++ Lab 10/lab10.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+
+#define SYSTEM_PAUSE system("pause");
+#define GO_TO_NEXT_LINE printf("\n");
 
 float *getMatrix(int rows, int columns)
 {
+    srand(time(0));
     float *matrix = (float *)malloc(sizeof(int) * rows * columns);
-    printf("Enter matrix:\n");
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
         {
-            scanf("%f", (matrix + i * columns + j));
+            *(matrix + i * columns + j) = -3.0 + 8.0 * rand() / (float)RAND_MAX;
         }
     }
     return matrix;
@@ -24,7 +28,7 @@ void showMatrix(float *matrix, int rows, int columns)
         {
             printf("%7.2f", *(matrix + i * columns + j));
         }
-        printf("\n");
+        GO_TO_NEXT_LINE
     }
 }
 
@@ -41,11 +45,11 @@ float *copyArray(float *source, int rows, int columns)
     return matrix;
 }
 
-void divideRow(float *matrix, float divider, int columns, int row)
+void divideRow(float *rowStart, float divider, int columns)
 {
     for (int i = 0; i < columns; i++)
     {
-        *(matrix + row * columns + i) /= divider;
+        *(rowStart + i) /= divider;
     }
 }
 
@@ -55,7 +59,7 @@ float *doGauss(float *matrix, int rows, int columns)
     for (int i = 0; i < rows; i++)
     {
         float divider = *(newMatrix + i * columns + i);
-        divideRow(newMatrix, divider, columns, i);
+        divideRow(newMatrix + i * columns, divider, columns);
         for (int k = i + 1; k < rows; k++)
         {
             float temp = *(newMatrix + k * columns + i);
@@ -89,31 +93,41 @@ void showRoots(float *roots, int rows)
     {
         printf("x%d: %f  ", i + 1, roots[i]);
     }
-    printf("\n");
+    GO_TO_NEXT_LINE
 }
 
 int main()
 {
+    clock_t timeBefore;
+    clock_t timeAfter;
+
     int rows;
     int columns;
     printf("Enter rows and columns: ");
     scanf("%d%d", &rows, &columns);
 
     float *matrix = getMatrix(rows, columns);
-    // float array[12] = {1.14 -2.15 -5.11 -4.16 -0.71 0.81 -0.02 -0.17 0.42 -1.13 7.05 6.15};
+    // {1.14 -2.15 -5.11 -4.16 -0.71 0.81 -0.02 -0.17 0.42 -1.13 7.05 6.15};
+
+    timeBefore = clock();
     float *newMatrix = doGauss(matrix, rows, columns);
+    float *roots = getRoots(newMatrix, rows, columns);
+    timeAfter = clock();
+
+    double delay = (double)(timeAfter - timeBefore) / CLOCKS_PER_SEC;
 
     printf("\nEntered matrix:\n");
     showMatrix(matrix, rows, columns);
-    printf("New matrix:\n");
+    printf("\nNew matrix:\n");
     showMatrix(newMatrix, rows, columns);
-
-    float *roots = getRoots(newMatrix, rows, columns);
+    printf("\nRoots: ");
     showRoots(roots, rows);
+    printf("\n\nDelay : %.30lf seconds\n", delay);
 
     free(roots);
     free(matrix);
     free(newMatrix);
 
+    SYSTEM_PAUSE
     return 0;
 }
