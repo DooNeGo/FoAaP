a325b3968d709c4100cb03cf23d6541e5ac5bcb2
Mathew <113514856+DooNeGo@users.noreply.github.com>
2023-04-11 00:14:40 +0300

    Delete Actions.h

--- Lab 12/Actions.h
+++
@@ -1,7 +0,0 @@
-#ifndef ACTIONS
-#define ACTIONS
-
-void swap(void *elemi, void *elemj, unsigned int size);
-void *concatenation(const void *Source, const void *Source1, const int SourceCount, const int Source1Count, const unsigned int singleElementSize, int *outNewCount);
-
-#endif
